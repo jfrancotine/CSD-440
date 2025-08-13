b3cc66fdeb8b1462de34ec3035175691f799d420
Jose Franco <jofranco@my365.bellevue.edu>
2025-08-12 23:37:00 -0500

    Push 08/12/25

---
+++ module-1/JoseFirstProgram.php
@@ -0,0 +1,58 @@
+<!--Jose Franco
+08/11/25
+Module 1 Programming Assignment
+Creating a simple PHP file -->
+
+<html>
+<head>
+    <title>JoseFirstProgram</title>
+    <!--Giving style to the text-->
+    <style>
+        body {
+            font-family: Arial;
+            color: #333;
+            max-width: 800px;
+            margin: 40px auto;
+            padding: 20px;
+            background: white;
+            
+        }
+        h1 {
+            text-align: center;
+            color: #0989f1ff;
+            margin-bottom: 20px;
+        }
+       
+    </style>
+</head>
+
+<body>
+    <h1>JoseFirstProgram</h1>
+
+<!--PHP code starts here-->
+    <?php echo nl2br("
+Hello everyone, I hope you are all doing well and had an amazing break. 
+My name is Jose Franco and I am originally from Venezuela. In 2017, I moved to Chile, and two years later, in 2019, I decided to move here to the United States. 
+I am currently living in Flower Mound, TX with my wife and our husky, Enzo.
+
+Professionally, I am pursuing my second bachelor’s degree in software development. My first degree back home was in business. 
+I currently work at Target as a team leader, and it has been a great experience and opportunity, especially since they are covering the cost of this program.
+
+In my free time, I enjoy serving at church, particularly in the Spanish ministry, and going bowling, a sport I’ve loved since growing up in Venezuela. 
+I was even able to compete professionally, participating in several state championships.
+
+I am very excited for this term, especially to learn more about PHP and creating dynamic web pages. 
+I use Discord often and have already made great connections with classmates, helping each other out along the way. 
+If you ever need a hand and I can help, please don’t hesitate to reach out — my username is jose_03186.
+
+My ultimate career goal is to join a tech company where I can apply everything I’ve learned so far. 
+I especially enjoy back-end development, but in the long run, I aim to grow into a full-stack developer. 
+Throughout this program, I have gained valuable experience in Java, Python, SQL, JavaScript, HTML, and CSS, 
+and I plan to use these skills to build efficient, scalable, and impactful applications that solve real-world problems.
+
+I wish you all the best in this new and for many last term before graduation.
+
+Jose");?>
+
+</body>
+</html>
